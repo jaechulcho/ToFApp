16ba95e28ae7f7fa7e4d7bef759a279b0e719cfa
josco <jccho@fine-tec.com>
2017-03-17 19:43:15 +0900

    기본 파일 인토딩을 UTF-8로 함.

--- polyinout/src/AreaData.cpp
+++ polyinout/src/AreaData.cpp
@@ -30,14 +30,11 @@ AreaPoint& AreaData::operator[](int index) {
 
 int AreaData::isInside(double x, double z) const
 {
-    // crosses는 점q와 오른쪽 반직선과 다각형과의 교점의 갯수
     int ret = 0;
     int crosses = 0;
     for (int i = 0; i < MAX_AP_NUM; i++) {
         int j = (i+1)%MAX_AP_NUM;
-        //점 B가 선분 (p[i], p[2])의 y좌표 사이에 있음
         if ((m_point[i].z > z) != (m_point[j].z > z)) {
-            //atX는 점B를 지나는 수평선과 선분 (p[i], p[j])의 교점
             double atX = (((m_point[j].x-m_point[i].x)*(z-m_point[i].z))/(m_point[j].z-m_point[i].z))
                             + m_point[i].x;
             if (x < atX) {
